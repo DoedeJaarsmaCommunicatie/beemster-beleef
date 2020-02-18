f6790081d0e133d5981de614f009107968b348c9
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2020-02-18 08:40:40 +0100

    Added modules for variables and functions

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
 <?php
 
 $context = \Timber\Timber::get_context();
+$context['post'] = new \Timber\Post();
 
-return \Timber\Timber::render(
-    [
-        'views/page.html.twig',
-    ],
-    $context
-);
+return \Timber\Timber::render([
+	\App\Helpers\Template::viewHtmlTwigFile('page'),
+],$context);
