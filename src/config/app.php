3899161590286cbb11a8f273ef127d263bc41682
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2020-02-19 16:16:51 +0100

    Added header and footer

--- src/config/app.php
+++ src/config/app.php
@@ -3,10 +3,12 @@ namespace App;
 
 use App\Providers\MenuServiceProvider;
 use App\Providers\AssetServiceProvider;
+use App\Providers\CarbonServiceProvider;
 
 return [
 	'providers'     => [
 	    MenuServiceProvider::class,
 		AssetServiceProvider::class,
+		CarbonServiceProvider::class
 	]
 ];
