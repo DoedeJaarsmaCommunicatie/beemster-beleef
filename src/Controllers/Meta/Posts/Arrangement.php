8fa1162f968d4855af91cd0bfbbe7cc550326f76
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2020-02-20 14:09:39 +0100

    Added meta to arrangements

--- src/Controllers/Meta/Posts/Arrangement.php
+++ src/Controllers/Meta/Posts/Arrangement.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Controllers\Meta\Posts;
 
 use Carbon_Fields\Field;
@@ -7,29 +8,47 @@ use App\Controllers\Meta\Meta;
 
 class Arrangement extends Meta
 {
-	public static function register ()
-	{
-		Container::make('post_meta', __('Arrangement informatie'))
-			->where('post_type', '=', 'arrangement')
-			->add_fields(static::fields());
-	}
+    public static function register()
+    {
+        Container::make('post_meta', __('Arrangement informatie'))
+            ->where('post_type', '=', 'arrangement')
+            ->add_fields(static::fields());
 
-	public static function fields (): array
-	{
-		$fields = [];
-		$fields []= Field::make('image', 'image', __('Featured image'));
+        Container::make('post_meta', __('Arrangement locaties'))
+            ->where('post_type', '=', 'arrangement')
+            ->add_fields(static::locationFields());
+    }
 
-		$fields []= Field::make('text', 'price', 'Prijs')
-			->set_attribute('placeholder', 'Bijvoorbeeld v.a. 23,00 p.p.')
-			->set_help_text('Het euro-teken kom op de site er bij');
+    public static function fields(): array
+    {
+        $fields = [];
+        $fields [] = Field::make('image', 'image', __('Featured image'));
 
-		$fields []= Field::make('select', 'time', __('Time'))
-			->set_options([
-				0 => '1,5 uur',
-				1 => 'Dagdeel',
-				2 => 'Hele dag'
-			]);
+        $fields [] = Field::make('text', 'price', 'Prijs')
+            ->set_attribute('placeholder', 'Bijvoorbeeld v.a. 23,00 p.p.')
+            ->set_help_text('Het euro-teken kom op de site er bij');
 
-		return $fields;
-	}
+        $fields [] = Field::make('select', 'time', __('Time'))
+            ->set_options([
+                '1,5 uur' => '1,5 uur',
+                'Dagdeel' => 'Dagdeel',
+                'Hele dag' => 'Hele dag'
+            ]);
+
+        return $fields;
+    }
+
+    public static function locationFields(): array
+    {
+        $fields = [];
+        $fields [] = Field::make('association', 'locations', __('Locatie'))
+            ->set_types([
+                [
+                    'type' => 'post',
+                    'post_type' => 'location'
+                ]
+            ]);
+
+        return $fields;
+    }
 }
