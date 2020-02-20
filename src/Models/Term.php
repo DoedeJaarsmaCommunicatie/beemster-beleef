ab8fed9f09b49d142b43b956d5854aa7305f263d
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2020-02-20 14:07:59 +0100

    Added a convert function to Terms to make attaching posts easier

--- src/Models/Term.php
+++ src/Models/Term.php
@@ -2,11 +2,36 @@
 
 namespace App\Models;
 
+use Timber\Helper;
 use Timber\Term as TaxTerm;
 
 class Term extends TaxTerm
 {
-	public function get_field( $field_name ) {
-		return carbon_get_post_meta($this->id, $field_name);
+	/**
+	 * Finds any WP_Post objects and converts them to Timber\Posts
+	 * @param array|WP_Post $data
+	 * @param string $class
+	 */
+	public function convert( $data ) {
+		if ( is_object($data) ) {
+			$data = Helper::convert_wp_object($data);
+		} else if ( is_array($data) ) {
+			$func = __FUNCTION__;
+			foreach ( $data as &$ele ) {
+				if ( is_array($ele) ) {
+					$ele = $this->$func($ele);
+				} else if ( is_object($ele) ) {
+					$ele = Helper::convert_wp_object($ele);
+				}
+			}
+		}
+		return $data;
+	}
+
+	public function get_field( $field_name )
+	{
+		$value = carbon_get_term_meta($this->id, $field_name);
+		$value = $this->convert($value);
+		return $value;
 	}
 }
