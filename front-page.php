a6274a2c90e9aa3fd0d6204f8df96aa4b681936c
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2020-02-20 08:58:45 +0100

    Added terms to fp, split fp up

--- front-page.php
+++ front-page.php
@@ -1,11 +1,21 @@
 <?php
 
-use Timber\Post;
+use App\Models\Post;
 use Timber\Timber;
+use App\Models\Term;
 use App\Helpers\Template;
 
 $context         = Timber::get_context();
 $context['post'] = new Post();
+$context['themes'] = Timber::get_terms([
+	'taxonomy' => 'theme',
+	'meta_query' => [
+		'key' => 'position',
+		'type' => 'NUMERIC'
+	],
+	'orderby' => 'meta_value_num',
+	'order' => 'ASC'
+], [], Term::class);
 
 return Timber::render([
 	Template::viewHtmlTwigFile('front-page'),
