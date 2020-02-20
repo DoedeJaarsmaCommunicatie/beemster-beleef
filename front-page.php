5493f01e8e924137b9acc04e9d1b058bbff43b17
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2020-02-20 14:14:53 +0100

    Added caching to front page data and used new components

--- front-page.php
+++ front-page.php
@@ -1,23 +1,35 @@
 <?php
 
+use Timber\Helper;
 use App\Models\Post;
 use Timber\Timber;
 use App\Models\Term;
+use Timber\PostQuery;
 use App\Helpers\Template;
+use DusanKasan\Knapsack\Collection;
 
 $context         = Timber::get_context();
 $context['post'] = new Post();
-$context['themes'] = Timber::get_terms([
-	'taxonomy' => 'theme',
-	'meta_query' => [
-		'key' => 'position',
-		'type' => 'NUMERIC'
-	],
-	'orderby' => 'meta_value_num',
-	'order' => 'ASC'
-], [], Term::class);
+$context['themes'] = Helper::transient('themes', static function () {
+    $themes = Timber::get_terms([
+        'taxonomy' => 'theme'
+    ], [], Term::class);
+
+    $themes = Collection::from($themes);
+
+    return $themes->sort(static function (Term $first, Term $second) {
+        return (int) $first->get_field('position') > (int) $second->get_field('position');
+    })->toArray();
+}, 3600);
+
+$context['arrangements'] = Helper::transient('arrangements', static function () {
+    return Collection::from(new PostQuery([
+        'post_type' => 'arrangement',
+        'posts_per_page' => 3
+    ], Post::class))->toArray();
+}, 600);
 
 return Timber::render([
-	Template::viewHtmlTwigFile('front-page'),
-	Template::viewHtmlTwigFile('page'),
-],$context);
+    Template::viewHtmlTwigFile('front-page'),
+    Template::viewHtmlTwigFile('page'),
+], $context);
