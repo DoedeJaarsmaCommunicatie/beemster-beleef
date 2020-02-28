1660d3507ab3562c3a8901b091561b1a16c82099
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2020-02-28 10:05:48 +0100

    Added filters and actions to front-page

--- front-page.php
+++ front-page.php
@@ -7,13 +7,19 @@ use Timber\Timber;
 use App\Helpers\Template;
 use App\Helpers\Transients\{ThemeTransients, ArrangementTransients};
 
+$templates = [
+	Template::viewHtmlTwigFile('front-page'),
+	Template::viewHtmlTwigFile('page'),
+];
+
 $context         = Timber::get_context();
 $context['post'] = new Post();
 
 $context['themes'] = ThemeTransients::getCachedThemes();
 $context['arrangements'] = ArrangementTransients::getFrontArrangements();
 
-Timber::render([
-    Template::viewHtmlTwigFile('front-page'),
-    Template::viewHtmlTwigFile('page'),
-], $context);
+Timber::render(
+	apply_filters('bdb/pages/front-page/templates', $templates),
+	apply_filters('bdb/pages/front-page/context', $context),
+	apply_filters('bdb/pages/front-page/cache/expire', [3600, false])
+);
