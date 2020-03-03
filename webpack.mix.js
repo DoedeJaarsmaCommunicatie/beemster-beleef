14256ed8747516de8f591e771791d7c5d16a8755
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2020-03-03 07:35:09 +0100

    Moved image from assets to dist

--- webpack.mix.js
+++ webpack.mix.js
@@ -20,6 +20,7 @@ const purger = mix.inProduction()
 mix
   .sass('assets/styles/main.scss', 'dist/styles/main.css')
   .js('assets/scripts/main.js', 'dist/scripts/main.js')
+  .copyDirectory('assets/images', 'dist/images')
   .options({
     processCssUrls: false,
     postCss: [require('tailwindcss'), require('autoprefixer'), ...purger],
